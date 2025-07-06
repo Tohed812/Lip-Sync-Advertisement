0676c350820ccf6f5694bfda2e01f67c288ef1ee
Tohed812 <tohedrakib761@gmail.com>
2025-07-06 22:05:21 +0600

    Fix MainContent alignment and improve layout structure

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -14,18 +14,21 @@ const VisuallyHiddenInput = styled("input")({
 	width: 1,
 });
 
-const MainContentWrapper = styled(Box)(({ theme }) => ({
+const MainContentWrapper = styled(Box)({
 	flexGrow: 1,
+	height: "100vh",
+	overflow: "auto",
+	paddingLeft: "240px", // Sidebar width
+	paddingRight: "350px", // RightPanel width
+});
+
+const ContentContainer = styled(Box)(({ theme }) => ({
+	maxWidth: "1000px",
+	margin: "0 auto",
 	padding: theme.spacing(3),
-	marginLeft: "240px", // Sidebar width
-	marginRight: "350px", // RightPanel width
 	display: "flex",
 	flexDirection: "column",
 	alignItems: "center",
-	maxWidth: "calc(100% - 590px)", // 240px (Sidebar) + 350px (RightPanel)
-	margin: "0 auto",
-	position: "relative",
-	left: "120px", // Half of Sidebar width to center the content
 }));
 
 const MainContent = () => {
@@ -52,146 +55,182 @@ const MainContent = () => {
 
 	return (
 		<MainContentWrapper>
-			<Typography variant="h5" gutterBottom align="center">
-				Create Advertisement
-			</Typography>
-
-			<Box
-				sx={{
-					display: "flex",
-					gap: 3,
-					mb: 3,
-					width: "100%",
-					justifyContent: "center",
-				}}
-			>
-				{/* Avatar Upload Section */}
-				<Paper
-					sx={{
-						p: 2,
-						flex: 1,
-						textAlign: "center",
-						maxWidth: "400px",
-					}}
-				>
-					<Typography variant="h6" gutterBottom>
-						Upload Avatar
-					</Typography>
-					{avatarImage ? (
-						<Box sx={{ position: "relative" }}>
-							<img
-								src={avatarImage}
-								alt="Avatar"
-								style={{
-									maxWidth: "100%",
-									maxHeight: "300px",
-									objectFit: "contain",
-								}}
-							/>
-							<Button
-								variant="contained"
-								size="small"
-								sx={{
-									position: "absolute",
-									bottom: 8,
-									right: 8,
-								}}
-								onClick={() => setAvatarImage(null)}
-							>
-								Remove
-							</Button>
-						</Box>
-					) : (
-						<Button
-							component="label"
-							variant="contained"
-							startIcon={<CloudUploadIcon />}
-						>
-							Upload Avatar
-							<VisuallyHiddenInput
-								type="file"
-								accept="image/*"
-								onChange={(e) => handleImageUpload(e, "avatar")}
-							/>
-						</Button>
-					)}
-				</Paper>
-
-				{/* Product Upload Section */}
-				<Paper
-					sx={{
-						p: 2,
-						flex: 1,
-						textAlign: "center",
-						maxWidth: "400px",
-					}}
-				>
-					<Typography variant="h6" gutterBottom>
-						Upload Product
-					</Typography>
-					{productImage ? (
-						<Box sx={{ position: "relative" }}>
-							<img
-								src={productImage}
-								alt="Product"
-								style={{
-									maxWidth: "100%",
-									maxHeight: "300px",
-									objectFit: "contain",
-								}}
-							/>
-							<Button
-								variant="contained"
-								size="small"
-								sx={{
-									position: "absolute",
-									bottom: 8,
-									right: 8,
-								}}
-								onClick={() => setProductImage(null)}
-							>
-								Remove
-							</Button>
-						</Box>
-					) : (
-						<Button
-							component="label"
-							variant="contained"
-							startIcon={<CloudUploadIcon />}
-						>
-							Upload Product
-							<VisuallyHiddenInput
-								type="file"
-								accept="image/*"
-								onChange={(e) =>
-									handleImageUpload(e, "product")
-								}
-							/>
-						</Button>
-					)}
-				</Paper>
-			</Box>
-
-			{/* Preview Section */}
-			<Paper sx={{ p: 2, mt: 3, width: "100%", maxWidth: "800px" }}>
-				<Typography variant="h6" gutterBottom align="center">
-					Preview
+			<ContentContainer>
+				<Typography variant="h5" gutterBottom align="center">
+					Create Advertisement
 				</Typography>
+
 				<Box
 					sx={{
-						height: "400px",
-						backgroundColor: "#f5f5f5",
 						display: "flex",
-						alignItems: "center",
+						gap: 3,
+						mb: 3,
+						width: "100%",
 						justifyContent: "center",
-						borderRadius: 1,
+						flexWrap: { xs: "wrap", md: "nowrap" },
 					}}
 				>
-					<Typography color="text.secondary">
-						Preview will appear here after processing
-					</Typography>
+					{/* Avatar Upload Section */}
+					<Paper
+						sx={{
+							p: 2,
+							flex: "1 1 300px",
+							maxWidth: "400px",
+							minWidth: { xs: "100%", md: "300px" },
+							textAlign: "center",
+						}}
+					>
+						<Typography variant="h6" gutterBottom>
+							Upload Avatar
+						</Typography>
+						{avatarImage ? (
+							<Box sx={{ position: "relative" }}>
+								<img
+									src={avatarImage}
+									alt="Avatar"
+									style={{
+										width: "100%",
+										height: "300px",
+										objectFit: "contain",
+									}}
+								/>
+								<Button
+									variant="contained"
+									size="small"
+									sx={{
+										position: "absolute",
+										bottom: 8,
+										right: 8,
+									}}
+									onClick={() => setAvatarImage(null)}
+								>
+									Remove
+								</Button>
+							</Box>
+						) : (
+							<Box
+								sx={{
+									height: "300px",
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									backgroundColor: "#f5f5f5",
+									borderRadius: 1,
+								}}
+							>
+								<Button
+									component="label"
+									variant="contained"
+									startIcon={<CloudUploadIcon />}
+								>
+									Upload Avatar
+									<VisuallyHiddenInput
+										type="file"
+										accept="image/*"
+										onChange={(e) =>
+											handleImageUpload(e, "avatar")
+										}
+									/>
+								</Button>
+							</Box>
+						)}
+					</Paper>
+
+					{/* Product Upload Section */}
+					<Paper
+						sx={{
+							p: 2,
+							flex: "1 1 300px",
+							maxWidth: "400px",
+							minWidth: { xs: "100%", md: "300px" },
+							textAlign: "center",
+						}}
+					>
+						<Typography variant="h6" gutterBottom>
+							Upload Product
+						</Typography>
+						{productImage ? (
+							<Box sx={{ position: "relative" }}>
+								<img
+									src={productImage}
+									alt="Product"
+									style={{
+										width: "100%",
+										height: "300px",
+										objectFit: "contain",
+									}}
+								/>
+								<Button
+									variant="contained"
+									size="small"
+									sx={{
+										position: "absolute",
+										bottom: 8,
+										right: 8,
+									}}
+									onClick={() => setProductImage(null)}
+								>
+									Remove
+								</Button>
+							</Box>
+						) : (
+							<Box
+								sx={{
+									height: "300px",
+									display: "flex",
+									alignItems: "center",
+									justifyContent: "center",
+									backgroundColor: "#f5f5f5",
+									borderRadius: 1,
+								}}
+							>
+								<Button
+									component="label"
+									variant="contained"
+									startIcon={<CloudUploadIcon />}
+								>
+									Upload Product
+									<VisuallyHiddenInput
+										type="file"
+										accept="image/*"
+										onChange={(e) =>
+											handleImageUpload(e, "product")
+										}
+									/>
+								</Button>
+							</Box>
+						)}
+					</Paper>
 				</Box>
-			</Paper>
+
+				{/* Preview Section */}
+				<Paper
+					sx={{
+						p: 2,
+						mt: 3,
+						width: "100%",
+						maxWidth: "900px",
+					}}
+				>
+					<Typography variant="h6" gutterBottom align="center">
+						Preview
+					</Typography>
+					<Box
+						sx={{
+							height: "400px",
+							backgroundColor: "#f5f5f5",
+							display: "flex",
+							alignItems: "center",
+							justifyContent: "center",
+							borderRadius: 1,
+						}}
+					>
+						<Typography color="text.secondary">
+							Preview will appear here after processing
+						</Typography>
+					</Box>
+				</Paper>
+			</ContentContainer>
 		</MainContentWrapper>
 	);
 };
